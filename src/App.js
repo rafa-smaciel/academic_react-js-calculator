68fa53f4250745e0c75a94fcffddb6e4c017208b
rafael <rafaelmdeveloper@gmail.com>
2022-04-22 20:35:05 -0300

    css window

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, {useState} from 'react'
 
 export default function App(){
+  
+  const [valorTela, setValorTela] = useState('')
+  const [resultado, setResultado] = useState(0)
+  const [acumulador, setAcumulador] = useState(0)
+  const [operacao, setOperacao] = useState(false)
 
+  const Tela=(valor,res)=>{
+    return(
+      <div style="">
+        <span style={}>{valor}</span>
+        <span style={}>{res}</span>
+      </div>
+    )
+  }
+
+  //Estilos
+  const cssTela={
+    display: 'flex',
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'felx-start',
+    backgroundColor: '#444',
+    flexDirection: 'column',
+    window: 260,
+  }
+  
     return (
       <>
       </>
