fe0eda2e6bfda1169ffc99fbc46a409cea1158fb
rafael <rafaelmdeveloper@gmail.com>
2022-04-22 21:26:15 -0300

    full return

--- src/App.js
+++ src/App.js
@@ -38,33 +38,31 @@ export default function App(){
     const valorDigitadoTela=valorTela+d
     setValorTela(valorDigitadoTela)
     }
-  }
-
-  const limparMemoria=()=>{
-    setOperado(false)
-    setValorTela('')
-    setResultado(0)
-    setAcumulador(0)
-    return
-  }
-
-  const Operacao=(oper)=>{
-    if(oper=='bs'){
-      let vtela=valorTela
-      vtela=vtela.substring(0,(vtela.length-1))
-      setValorTela(vtela)
+    const limparMemoria=()=>{
       setOperado(false)
+      setValorTela('')
+      setResultado(0)
+      setAcumulador(0)
       return
     }
-    try{
-      const r=eval(valorTela)
-      setAcumulador(r)
-      setResultado(r)
-      setOperado(true)
-    }catch{
-      setResultado('ERRO')
+
+    const Operacao=(oper)=>{
+      if(oper=='bs'){
+        let vtela=valorTela
+        vtela=vtela.substring(0,(vtela.length-1))
+        setValorTela(vtela)
+        setOperado(false)
+        return
+      }
+      try{
+        const r=eval(valorTela)
+        setAcumulador(r)
+        setResultado(r)
+        setOperado(true)
+      }catch{
+        setResultado('ERRO')
+      }
     }
-  }
 
   //ESTILOS
   const cssContainer={
@@ -89,7 +87,7 @@ export default function App(){
     alignItems: 'felx-start',
     backgroundColor: '#444',
     flexDirection: 'column',
-    window: 260
+    width: 260
   }
   
   const cssTelaOper={
@@ -115,9 +113,34 @@ export default function App(){
     outline: 'none'
   }
 
-    return (
-      <>
-        <div style={cssContainer}>
-      </>
-    );
+  return (
+    <>
+      <div style={cssContainer}>
+        <h3>Calculadora Matemática Simples</h3>
+        {Tela(valorTela,resultado)}
+        <div style={cssBotoes}>
+          {Btn('AC',()=>limparMemoria)}
+          {Btn('(',()=>addDigitoTela('('))}
+          {Btn(')',()=>addDigitoTela(')'))}
+          {Btn('/',()=>addDigitoTela('/'))}
+          {Btn('7',()=>addDigitoTela('7'))}
+          {Btn('8',()=>addDigitoTela('8'))}
+          {Btn('9',()=>addDigitoTela('9'))}
+          {Btn('*',()=>addDigitoTela('*'))}
+          {Btn('4',()=>addDigitoTela('4'))}
+          {Btn('5',()=>addDigitoTela('5'))}
+          {Btn('6',()=>addDigitoTela('6'))}
+          {Btn('-',()=>addDigitoTela('-'))}
+          {Btn('1',()=>addDigitoTela('1'))}
+          {Btn('2',()=>addDigitoTela('2'))}
+          {Btn('3',()=>addDigitoTela('3'))}
+          {Btn('+',()=>addDigitoTela('+'))}
+          {Btn('0',()=>addDigitoTela('0'))}
+          {Btn('.',()=>addDigitoTela('.'))}
+          {Btn('<-',()=>Operacao('bs'))}
+          {Btn('=',()=>Operacao('='))}
+        </div>
+      </div>
+    </>
+  );
 }
